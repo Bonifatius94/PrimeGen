c918be3d8ecc1252cf53677f5279d0840f5864c2
Marco Tröster <marco@troester-gmbh.de>
2021-06-13 01:51:35 +0200

    fixed error at computing d

--- Program.cs
+++ Program.cs
@@ -9,17 +9,17 @@ namespace PrimeGen
         public static void Main(string[] args)
         {
             // define the key length to be generated
-            int keylen = 16;
+            int keylen = 128;
 
             // generate prime numbers and use them for some RSA
             var p = KeyGenUtils.GeneratePrime(keylen);
             var q = KeyGenUtils.GeneratePrime(keylen);
-            Console.WriteLine($"p={ p }, q={ q }");
+            Console.WriteLine($"p={ p }\nq={ q }");
 
             // compute N and phi(N)
             var N = p * q;
             var phiN = (p - 1) * (q - 1);
-            Console.WriteLine($"N={ N }, phi(N)={ phiN }");
+            Console.WriteLine($"N={ N }\nphi(N)={ phiN }");
 
             // choose e such that it is rel. prime to phi(N) and within 1 < e < phi(N)
             // then, determine d as mult. inverse of e, i.e. d * e = 1 (mod phi(N))
@@ -27,17 +27,14 @@ namespace PrimeGen
             do {
                 e = KeyGenUtils.GeneratePrime(keylen);
                 d = computeMultInverse(phiN, e);
-            }
-            while (e >= phiN || e == 1 || d < 0);
-
-            Console.WriteLine($"e={ e }, d={ d }");
-            Console.WriteLine($"{ e } * { d } (mod { phiN }) = { e * d % phiN }");
+            } while (e >= phiN || e == 1 || d < 0);
+            Console.WriteLine($"e={ e }\nd={ d }");
 
             // create a message and encrypt it
-            string message = "123";
+            string message = "Hello World, RSA encryption!";
             var plain = Encoding.ASCII.GetBytes(message);
             var cipher = BigInteger.ModPow(new BigInteger(plain), e, N).ToByteArray();
-            Console.WriteLine($"original message: '{ message }'");
+            Console.WriteLine($"original message:  '{ message }'");
             Console.WriteLine($"encrypted message: '{ Encoding.ASCII.GetString(cipher) }'");
 
             // decrypt the message again using RSA
@@ -62,8 +59,6 @@ namespace PrimeGen
             // loop until a mod b = 0
             while (b > 0)
             {
-                Console.WriteLine($"a={ a }, b={ b }");
-
                 // compute q = a div b and r = a mod b
                 q = a / b;
                 var r = a % b;
@@ -79,12 +74,10 @@ namespace PrimeGen
                 v = t;
                 s = sNew;
                 t = tNew;
-
-                Console.WriteLine($"q={ q }, r={ r }, u={ u }, v={ v }, s={ s }, t={ t }");
             }
 
-            // determine d: 1 = u * phi(N) + v * e => v = d
-            var d = v;
+            // determine d: 1 = v * phi(N) + u * e ~> u = d
+            var d = u;
 
             // only return d if GCN(a, b) = 1, otherwise d=-1
             return a == 1 ? d : -1;
